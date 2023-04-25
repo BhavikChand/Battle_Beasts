ef432aefc0a7c14bc7ddadedf532644808a8d7ba
Bhavik Chand <hyruka98@gmail.com>
2023-04-25 16:05:56 -0700

    Initial Commit

---
+++ app/src/main/java/com/example/battlebeasts/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.battlebeasts;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    //TODO Delete Me
+  }
+}
